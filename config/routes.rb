280d1cb1c2fb870d5ac6ada6e19063d0685cd41f
essxiv <essxiv@gmail.com>
2015-09-29 13:13:08 -0700

    set route contraints

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
-MarketPlaceApi::Application.routes.draw do
+MarketApi::Application.routes.draw do
+  # API Definition
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api'}, path: '/' do
+    # List Resources
+  end
 
 end
