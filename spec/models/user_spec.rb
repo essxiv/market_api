fa0003c6b21130dc3033e1d27bf14bebd9153c9e
essxiv <essxiv@gmail.com>
2015-09-29 15:24:41 -0700

    Added shoulda-matchers

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,7 @@ describe User do
     it { should respond_to(:email) }
     it { should respond_to(:password) }
     it { should respond_to(:password_confirmation) }
+    it { should allow_value('example@domain.com').for(:email) }
 
     it { should be_valid }
   # pending "add some examples to (or delete) #{__FILE__}"
