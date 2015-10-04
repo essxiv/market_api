6e28ff28df0c876979924bc1be51f2021b9d6752
essxiv <essxiv@gmail.com>
2015-10-04 14:43:26 -0700

    updated calculation for order

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,12 +3,16 @@ class Order < ActiveRecord::Base
 
   validates :total, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :user_id, presence: true
+  validates_with EnoughProductsValidator
 
   has_many :placements
   has_many :products, through: :placements
 
   def set_total!
-    self.total = products.map(&:price).sum
+    self.total = 0 
+    placements.each do |p|
+        self.total += p.product.price * p.quantity
+    end
   end
 
   def order_params
